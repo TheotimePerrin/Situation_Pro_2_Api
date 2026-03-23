cf766f2bc303de6e8f21d84ef8c071eb75192ee8
MANSAR Jessim <jessim.mansar@GB42.local>
2026-03-23 09:09:24 +0100

    mise a jour

--- WoodyCraftWeb/routes/api.php
+++ WoodyCraftWeb/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PuzzleController;
+use App\Http\Controllers\Api\StockController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,3 +19,8 @@ Route::apiResource('puzzles', PuzzleController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Routes dédiées au stock
+Route::get('/stocks', [StockController::class, 'index']);           // pour tout voir 
+Route::patch('/stocks/{id}', [StockController::class, 'update']);   // pour pouvoir modifier 
